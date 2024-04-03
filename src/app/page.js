d57510126ab77d53922b146341e580d70a986bb7
Faheem-Siddiqi <faheem_siddiqi@yahoo.com>
2024-04-03 12:14:34 +0500

    Minor

--- src/app/page.js
+++ src/app/page.js
@@ -5,13 +5,13 @@ import Join from "@/components/home/Join";
 import PrimeServices from "@/components/home/PrimeServices";
 import FeaturedOfficeSpace from "@/components/home/FeaturedOfficeSpace";
 import ExploreMayfair from "@/components/home/ExploreMayfair";
-import ScrollTop from "@/components/layout/ScrollTop"
+
 import Chat from "@/components/layout/Chat";
 export default function Home() {
   return (
     <>
-      <ScrollTop />
-      <Chat />
+
+
       <HeroSection />
       <ExploreMayfair />
       <FeaturedOfficeSpace />
